63ea0c4efeeafe703643589b55ffa72a5349f5bb
Code-Maniac-Rza <rzashahryar@gmail.com>
2025-01-07 15:23:26 +0530

    updated database

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(default='postgresql://tweet_app_36qf_user:nruOlvg3Ef1NYmOHfnCJ08CDXzBahY6E@dpg-ctufeqt2ng1s739dinug-a.oregon-postgres.render.com/tweet_app_36qf')
 }
 
 
